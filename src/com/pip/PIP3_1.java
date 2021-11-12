1ccfbd6ef3d5e8b8c009c16af642a2835ef8d09b
s26124 <s26124@pjwstk.edu.pl>
2021-11-12 23:22:51 +0100

    Task 1 of PIP3

--- src/com/pip/PIP3_1.java
+++ src/com/pip/PIP3_1.java
@@ -1,4 +1,37 @@
 package com.pip;
+import java.util.*;
 
 public class PIP3_1 {
+    public static void main(String[] args) {
+    String imie, nazwisko, plec;
+        System.out.print("Podaj imię: ");
+    Scanner scan = new Scanner(System.in);
+    imie = scan.next();
+        System.out.print("Podaj nazwisko: ");
+    nazwisko = scan.next();
+        System.out.print("Podaj płeć (mężczyzna/kobieta): ");
+    plec = scan.next();
+    wypisz(imie,nazwisko,plec);
+
+    //String w = "Witaj ";
+    // String scan = w + imie.nextLine();
+    // System.out.println();
+
+}
+
+    public static void wypisz(String imie, String nazwisko, String plec)
+    {
+        String napis="";
+        if (plec.equals("mężczyzna")) { napis += "Student: ";}
+        else {
+            if (plec.equals("kobieta")) { napis += "Studentka: ";}
+            else {
+                napis += "Student/ka: ";
+            }
+        }
+
+
+        napis=napis+imie+" "+ nazwisko;
+        System.out.println(napis);
+    }
 }
