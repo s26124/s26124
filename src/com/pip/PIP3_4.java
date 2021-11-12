3ecdf325c80e547cfa26f8f1586e850975d95057
s26124 <s26124@pjwstk.edu.pl>
2021-11-12 23:43:28 +0100

    Task 4 of PIP3

---
+++ src/com/pip/PIP3_4.java
@@ -0,0 +1,16 @@
+package com.pip;
+import java.util.Scanner;
+
+public class PIP3_4 {
+
+    public static void main(String[] args) {
+
+        System.out.print("Podaj liczbę sztuk zakupionego towaru: ");
+        Scanner LiczbaSzt = new Scanner(System.in);
+        int Cena=10;
+
+        System.out.print("Czy jest promocja: ");
+
+    }
+
+}
