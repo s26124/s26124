71288ee3eb4cac38b243e2d46468e66112e3a276
s26124 <s26124@pjwstk.edu.pl>
2021-11-14 11:32:49 +0100

    Task 4 of PIP3

--- src/com/pip/PIP3_4.java
+++ src/com/pip/PIP3_4.java
@@ -6,11 +6,23 @@ public class PIP3_4 {
     public static void main(String[] args) {
 
         System.out.print("Podaj liczbę sztuk zakupionego towaru: ");
-        Scanner LiczbaSzt = new Scanner(System.in);
-        int Cena=10;
+        Scanner scan = new Scanner(System.in);
+        int Ilosc = scan.nextInt();
+        double CenaPocz=10;
+        double CenaPoRabacie;
 
-        System.out.print("Czy jest promocja: ");
+        System.out.print("Czy jest promocja: (TAK/NIE)");
+        String Promocja = scan.next();
 
+        if (Promocja.equals("TAK") && Ilosc>10) {CenaPoRabacie=CenaPocz/2; //System.out.println("Uwzględniono rabat 50%: " );
+        }
+            else {if (Promocja.equals("TAK") && Ilosc<10) {CenaPoRabacie=CenaPocz;}
+                else {CenaPoRabacie=CenaPocz*1.1;}}
+
+// W diagramie nie ma informacji o wypisaniu wartości ani cen przed/po rabacie.
+       //System.out.println("Wartość zakupu: "+ CenaPocz*Ilosc);
+       //System.out.println("Wartość po uwzględnieniu rabatu: "+ CenaPoRabacie*Ilosc);
+       // System.out.println(Promocja + " "+CenaPocz+" po rabacie: "+CenaPoRabacie);
     }
 
 }
